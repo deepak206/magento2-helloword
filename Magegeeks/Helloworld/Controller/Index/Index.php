ce5150213e8078ceb004d59459ada79ef5498143
mss <ymss.deepakmankotia@gmail.com>
2016-09-17 12:19:26 +0530

    Magegeeks Hello World Magento2 Module

---
+++ Magegeeks/Helloworld/Controller/Index/Index.php
@@ -0,0 +1,9 @@
+<?php
+namespace Magegeeks\Helloworld\Controller\Index;
+class Index extends \Magento\Framework\App\Action\Action
+{
+    public function execute()
+    {
+        echo 'Hello world!';
+    }
+}
